67c1b7385c45bc8e3b5f83be5dbbab5be11ed506
Stefan Hahnl <hahnl.stefan@gmail.com>
2025-01-12 20:56:31 +0100

    feat: add pmd config, update gradle, update application according pmd check; related to #7

--- src/main/java/bswe/swm2/accountservice/AccountserviceApplication.java
+++ src/main/java/bswe/swm2/accountservice/AccountserviceApplication.java
@@ -3,10 +3,13 @@ package bswe.swm2.accountservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Initial class for the application
+ */
 @SpringBootApplication
 public class AccountserviceApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(AccountserviceApplication.class, args);
 	}
 
